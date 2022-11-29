2f36ded10b989944cee54f6342060b1b7c7078e6
msavir <maayan@aptoslabs.com>
2022-11-29 18:40:27 +0000

    Deploying to gh-pages from @ aptos-labs/aptos-wallet-adapter@cfb18002eaaf68c1cb96377b54f8ec9cf1316930 🚀

---
+++ _next/static/chunks/pages/App-33dd40ca00f4d74a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[35],{2938:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/App",function(){return u(2819)}])}},function(n){n.O(0,[680,837,84,774,888,179],function(){return n(n.s=2938)}),_N_E=n.O()}]);
