c969bc1129e1c7b3fad4fa70d5c2caf74331b97f
maayan <maayansavir@gmail.com>
2022-12-19 14:37:31 +0200

    Updates

--- aptos-wallet-adapter/_next/static/B4xtuhgLMxN-3cwv9m36G/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-619172d290d1b745.js"],"/_error":["static/chunks/pages/_error-d09689b8b67cb332.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
