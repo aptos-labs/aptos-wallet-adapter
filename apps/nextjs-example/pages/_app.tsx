3fcf49fe1a612fdf13dbb849540b1cdaa5008372
maayan <maayansavir@gmail.com>
2023-01-06 17:25:01 +0200

    clean up

--- apps/nextjs-example/pages/_app.tsx
+++ apps/nextjs-example/pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from "next/app";
-
 import { AppContext } from "../components/AppContext";
 
-require("@aptos-labs/wallet-adapter-ant-design/dist/index.css");
-require("../styles/global.css");
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
