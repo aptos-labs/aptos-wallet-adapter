41b9f90fb8ffad268b263884b31052714e37e8b2
maayan <maayansavir@gmail.com>
2023-01-06 17:26:51 +0200

    clean up

--- apps/nextjs-example/pages/index.tsx
+++ apps/nextjs-example/pages/index.tsx
@@ -108,7 +108,7 @@ export default function App() {
         <SuccessAlert text={successAlertMessage} />
       )}
       {errorAlertMessage.length > 0 && <ErrorAlert text={errorAlertMessage} />}
-      <h1 className=" flex justify-center mt-2 mb-4 tw-text-4xl font-extrabold tracking-tight leading-none text-black">
+      <h1 className=" flex justify-center mt-2 mb-4 text-4xl font-extrabold tracking-tight leading-none text-black">
         Aptos Wallet Adapter Demo (Devnet)
       </h1>
       <table className="table-auto w-full border-separate border-spacing-y-8 shadow-lg bg-white border-separate">
