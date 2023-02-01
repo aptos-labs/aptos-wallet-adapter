88feafa40756799fa2ab75c94858158bc7543099
jillxuu <jill@aptoslabs.com>
2023-02-01 14:16:29 -0800

    Updates

--- _next/static/5i8Hj63Yx3AuVNMeWfUq9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/310-b66ce8bd6c461444.js","static/chunks/pages/index-68b801edeb4d4ad7.js"],"/_error":["static/chunks/pages/_error-d09689b8b67cb332.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
