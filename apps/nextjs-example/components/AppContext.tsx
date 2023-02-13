232242340bf787cd209ce50332e7252367d2ec82
Maayan <maayan@aptoslabs.com>
2023-02-13 20:51:14 +0200

    remove spika wallet support (#83)

--- apps/nextjs-example/components/AppContext.tsx
+++ apps/nextjs-example/components/AppContext.tsx
@@ -3,13 +3,15 @@ import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
 import { PontemWallet } from "@pontem/wallet-adapter-plugin";
 import { TrustWallet } from "@trustwallet/aptos-wallet-adapter";
-import { SpikaWallet } from "@spika/aptos-plugin";
-import { AptosWalletAdapterProvider, NetworkName } from "@aptos-labs/wallet-adapter-react";
+import {
+  AptosWalletAdapterProvider,
+  NetworkName,
+} from "@aptos-labs/wallet-adapter-react";
 import { FewchaWallet } from "fewcha-plugin-wallet-adapter";
 import { MSafeWalletAdapter } from "msafe-plugin-wallet-adapter";
 import { BloctoWallet } from "@blocto/aptos-wallet-adapter-plugin";
 import { WelldoneWallet } from "@welldone-studio/aptos-wallet-adapter";
-import { NightlyWallet } from '@nightlylabs/aptos-wallet-adapter-plugin'
+import { NightlyWallet } from "@nightlylabs/aptos-wallet-adapter-plugin";
 
 import {
   AutoConnectProvider,
@@ -26,12 +28,14 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     new RiseWallet(),
     new PontemWallet(),
     new TrustWallet(),
-    new SpikaWallet(),
     new FewchaWallet(),
     new MSafeWalletAdapter(),
     new NightlyWallet(),
     // Blocto supports Testnet/Mainnet for now.
-    new BloctoWallet({ network: NetworkName.Testnet, bloctoAppId: "6d85f56e-5f2e-46cd-b5f2-5cf9695b4d46" }),
+    new BloctoWallet({
+      network: NetworkName.Testnet,
+      bloctoAppId: "6d85f56e-5f2e-46cd-b5f2-5cf9695b4d46",
+    }),
     new WelldoneWallet(),
   ];
 
