b56882d124d1a0237289dc875f84f82d3b3f44fa
maayan <maayansavir@gmail.com>
2023-05-11 13:52:24 -0700

    revert dapp changes

--- apps/nextjs-example/components/WalletButtons.tsx
+++ apps/nextjs-example/components/WalletButtons.tsx
@@ -1,43 +1,22 @@
-import {
-  useWallet,
-  WalletReadyState,
-  isdRedirectable,
-  isMobile,
-  isInAppBrowser,
-  Wallet,
-} from "@aptos-labs/wallet-adapter-react";
+import { useWallet, WalletReadyState } from "@aptos-labs/wallet-adapter-react";
 
 const WalletButtons = () => {
   const { wallets, connect } = useWallet();
 
   return (
     <>
-      {wallets.map((wallet: Wallet) => {
-        const isWalletReady =
-          wallet.readyState === WalletReadyState.Installed ||
-          wallet.readyState === WalletReadyState.Loadable;
-        return !isWalletReady && isdRedirectable() && !wallet.deepLink ? (
+      {wallets.map((wallet) => {
+        const isWalletReady = wallet.readyState === WalletReadyState.Installed || wallet.readyState === WalletReadyState.Loadable
+        return (
           <button
-            className={`bg-blue-500 text-white font-bold py-2 px-4 rounded mr-4 opacity-50 cursor-not-allowed`}
-            disabled={true}
-            key={wallet.name}
-          >
-            <>{wallet.name} - Desktop Only</>
-          </button>
-        ) : (
-          <button
-            className={`bg-blue-500  text-white font-bold py-2 px-4 rounded mr-4 ${
-              isWalletReady
-                ? "hover:bg-blue-700"
-                : "opacity-50 cursor-not-allowed"
-            }`}
+            className={`bg-blue-500  text-white font-bold py-2 px-4 rounded mr-4 ${isWalletReady ? "hover:bg-blue-700" : "opacity-50 cursor-not-allowed"}`}
             disabled={!isWalletReady}
             key={wallet.name}
             onClick={() => connect(wallet.name)}
           >
             <>{wallet.name}</>
           </button>
-        );
+        )
       })}
     </>
   );
