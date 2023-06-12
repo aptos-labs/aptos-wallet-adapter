1672a0e375743407af17a9a8a81069102913d056
Maayan <maayan@aptoslabs.com>
2023-06-12 20:39:45 +0300

    Add mobile view support on ant design wallet selector modal (#146)
    * mobile support on ant design wallet selector modal
    
    * add changelog

---
+++ apps/nextjs-example/components/WalletSelectorAntDesign.tsx
@@ -0,0 +1,7 @@
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+
+const WalletSelectorAntDesign = () => {
+  return <WalletSelector />;
+};
+
+export default WalletSelectorAntDesign;
