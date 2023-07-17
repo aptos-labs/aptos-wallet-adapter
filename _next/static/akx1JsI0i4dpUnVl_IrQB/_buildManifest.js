8652d563e7e1f413dfd709a5a3e0039bb4442ab7
maayan <maayansavir@gmail.com>
2023-07-17 14:02:13 -0400

    Updates

---
+++ _next/static/akx1JsI0i4dpUnVl_IrQB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/252-678b177b93229ea8.js","static/chunks/pages/index-9adbccbfdf01eac4.js"],"/_error":["static/chunks/pages/_error-f9d64273a2a68b8c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
