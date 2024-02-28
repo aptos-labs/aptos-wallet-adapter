f87838ec45adf216551fc71841abf282564e8867
maayan <maayansavir@gmail.com>
2024-02-28 12:44:17 -0500

    Updates

---
+++ standard-example/_next/static/E9-VZOhFpy4u833hrcViL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/798-8826d273b2aab862.js","static/chunks/pages/index-01d4c1eb86445d29.js"],"/_error":["static/chunks/pages/_error-e7132482a1daf6f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
