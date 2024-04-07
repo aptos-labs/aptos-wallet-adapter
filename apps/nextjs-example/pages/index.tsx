1848ddf8496b9c1b2be07ac52e2d99dd3f5f9615
Rowan Douglas <rowan.james.douglas@gmail.com>
2024-04-07 16:50:31 +0100

    allowed non-lower case network name

--- apps/nextjs-example/pages/index.tsx
+++ apps/nextjs-example/pages/index.tsx
@@ -138,9 +138,8 @@ function WalletProps(props: {
 }) {
   const { account, network, wallet } = props;
   const isValidNetworkName = () => {
-    // TODO: Do we allow non lowercase
     return Object.values<string | undefined>(Network).includes(
-      props.network?.name
+      props.network?.name.toLowerCase()
     );
   };
 
