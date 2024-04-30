c1244777ad0599aae3aa6e3f417e573e1a5651d4
maayan <maayansavir@gmail.com>
2024-04-30 12:04:26 -0400

    Updates

---
+++ nextjs-example-testing/_next/static/chunks/pages/_error-13068e136bf65866.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{5902:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(3086)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5902)}),_N_E=n.O()}]);
