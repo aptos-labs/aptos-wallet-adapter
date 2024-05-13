19f4fdd981c00e6b6b5d5f02436a7d5448911963
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2024-05-13 14:57:11 -0700

    [core] Expose version into window object, if available (#277)

--- packages/wallet-adapter-core/src/index.ts
+++ packages/wallet-adapter-core/src/index.ts
@@ -1,5 +1,11 @@
+import { WALLET_ADAPTER_CORE_VERSION } from './version';
+
 export { WalletCore } from "./WalletCore";
 export * from "./LegacyWalletPlugins";
 export * from "./constants";
 export * from "./utils";
 export * from "./AIP62StandardWallets";
+
+if (typeof window !== 'undefined') {
+  (window as any).WALLET_ADAPTER_CORE_VERSION = WALLET_ADAPTER_CORE_VERSION;
+}
