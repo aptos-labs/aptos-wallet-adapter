095a39e9f9293cac6a3960dda188e8c53199b078
maayan <maayansavir@gmail.com>
2024-06-05 09:56:20 -0400

    Updates

--- nextjs-example-testing/_next/static/hUhIzZwSPdihLgJVujVMt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/712-005821861865e7c6.js","static/chunks/pages/index-0d06eb00cf815e95.js"],"/_error":["static/chunks/pages/_error-53113eac217c806c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
