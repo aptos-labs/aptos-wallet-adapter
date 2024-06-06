cdb34dd8b5776e49818595bb7ba93013793ed5b2
maayan <maayansavir@gmail.com>
2024-06-05 20:04:15 -0400

    Trigger release to update WALLET_ADAPTER_CORE_VERSION injected variable

--- packages/wallet-adapter-core/src/index.ts
+++ packages/wallet-adapter-core/src/index.ts
@@ -6,6 +6,7 @@ export * from "./constants";
 export * from "./utils";
 export * from "./AIP62StandardWallets";
 
+// inject adapter core version to the window
 if (typeof window !== "undefined") {
   (window as any).WALLET_ADAPTER_CORE_VERSION = WALLET_ADAPTER_CORE_VERSION;
 }
