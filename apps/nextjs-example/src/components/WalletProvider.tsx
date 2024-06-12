bb34d77bb38e4e038da3fb823ec8f5f2f3ffcd12
maayan <maayansavir@gmail.com>
2024-06-12 16:31:48 -0400

    use Petra aip-62 and remove IdentityConnect

--- apps/nextjs-example/src/components/WalletProvider.tsx
+++ apps/nextjs-example/src/components/WalletProvider.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { Network } from "@aptos-labs/ts-sdk";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { BitgetWallet } from "@bitget-wallet/aptos-wallet-adapter";
-import { IdentityConnectWallet } from "@identity-connect/wallet-adapter-plugin";
 import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
 import { MSafeWalletAdapter } from "@msafe/aptos-wallet-adapter";
 import { OKXWallet } from "@okwallet/aptos-wallet-adapter";
 import { PontemWallet } from "@pontem/wallet-adapter-plugin";
 import { TrustWallet } from "@trustwallet/aptos-wallet-adapter";
 import { FewchaWallet } from "fewcha-plugin-wallet-adapter";
-import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { PropsWithChildren } from "react";
 import { useAutoConnect } from "./AutoConnectProvider";
 import { useToast } from "./ui/use-toast";
@@ -20,9 +17,6 @@ export const WalletProvider = ({ children }: PropsWithChildren) => {
   const { toast } = useToast();
 
   const wallets = [
-    new IdentityConnectWallet("57fa42a9-29c6-4f1e-939c-4eefa36d9ff5", {
-      networkName: Network.TESTNET,
-    }),
     new BitgetWallet(),
     new FewchaWallet(),
     new MartianWallet(),
@@ -30,7 +24,6 @@ export const WalletProvider = ({ children }: PropsWithChildren) => {
     new PontemWallet(),
     new TrustWallet(),
     new OKXWallet(),
-    new PetraWallet(),
   ];
 
   return (
