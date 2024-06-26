168beb4525b3787b420acbff22cbbdffabfe0753
maayan <maayansavir@gmail.com>
2024-06-25 23:40:26 -0400

    use isAptosConnectWallet

--- packages/wallet-adapter-core/src/utils/aptosConnect.ts
+++ packages/wallet-adapter-core/src/utils/aptosConnect.ts
@@ -1,3 +1,4 @@
+import { AptosStandardWallet } from "../AIP62StandardWallets";
 import { WalletInfo } from "../LegacyWalletPlugins";
 import { AnyAptosWallet } from "../WalletCore";
 import { partitionWallets } from "./helpers";
@@ -10,7 +11,9 @@ export const APTOS_CONNECT_ACCOUNT_URL =
   "https://aptosconnect.app/dashboard/main-account";
 
 /** Returns `true` if the provided wallet is an Aptos Connect wallet. */
-export function isAptosConnectWallet(wallet: WalletInfo | AnyAptosWallet) {
+export function isAptosConnectWallet(
+  wallet: WalletInfo | AnyAptosWallet | AptosStandardWallet
+) {
   return wallet.url.startsWith(APTOS_CONNECT_BASE_URL);
 }
 
