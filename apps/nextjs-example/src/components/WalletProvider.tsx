fe55dd1333681815f9066725c2a1714e558402b7
ice <ice@mizu.io>
2024-07-31 19:14:10 +0800

    update: Mizu Wallet support

--- apps/nextjs-example/src/components/WalletProvider.tsx
+++ apps/nextjs-example/src/components/WalletProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AptosWalletAdapterProvider, NetworkName } from "@aptos-labs/wallet-adapter-react";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { BitgetWallet } from "@bitget-wallet/aptos-wallet-adapter";
 import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
 import { MSafeWalletAdapter } from "@msafe/aptos-wallet-adapter";
@@ -12,8 +12,6 @@ import { PropsWithChildren } from "react";
 import { Network } from "@aptos-labs/ts-sdk";
 import { useAutoConnect } from "./AutoConnectProvider";
 import { useToast } from "./ui/use-toast";
-import { MizuWallet } from '@mizuwallet-sdk/aptos-wallet-adapter'
-
 
 export const WalletProvider = ({ children }: PropsWithChildren) => {
   const { autoConnect } = useAutoConnect();
@@ -26,18 +24,21 @@ export const WalletProvider = ({ children }: PropsWithChildren) => {
     new MSafeWalletAdapter(),
     new PontemWallet(),
     new TrustWallet(),
-    new OKXWallet(), 
-    new MizuWallet({
-      appId:'dfa1794e-8412-4c9c-b28d-25ecd4c0b52f',
-      network: NetworkName.Testnet
-    })
+    new OKXWallet(),
   ];
 
   return (
     <AptosWalletAdapterProvider
       plugins={wallets}
       autoConnect={autoConnect}
-      dappConfig={{ network: Network.DEVNET, aptosConnectDappId: "57fa42a9-29c6-4f1e-939c-4eefa36d9ff5" }}
+      dappConfig={{
+        network: Network.TESTNET,
+        aptosConnectDappId: "57fa42a9-29c6-4f1e-939c-4eefa36d9ff5",
+        mizuwallet: {
+          manifestURL:
+            "https://assets.mz.xyz/static/config/mizuwallet-connect-manifest.json",
+        },
+      }}
       onError={(error) => {
         toast({
           variant: "destructive",
--- packages/wallet-adapter-core/src/AIP62StandardWallets/sdkWallets.ts
+++ packages/wallet-adapter-core/src/AIP62StandardWallets/sdkWallets.ts
@@ -1,15 +1,21 @@
-import { DevTWallet, TWallet } from "@atomrigslab/aptos-wallet-adapter";
 import { AptosConnectWallet } from "@aptos-connect/wallet-adapter-plugin";
-import { AptosStandardWallet } from "./WalletStandard";
 import { Network } from "@aptos-labs/ts-sdk";
+import { DevTWallet, TWallet } from "@atomrigslab/aptos-wallet-adapter";
+import { MizuWallet } from "@mizuwallet-sdk/aptos-wallet-adapter";
 import { DappConfig } from "../WalletCore";
+import { AptosStandardWallet } from "./WalletStandard";
 
 export function getSDKWallets(dappConfig?: DappConfig) {
   const sdkWallets: AptosStandardWallet[] = [];
 
   // Need to check window is defined for AptosConnect
   if (typeof window !== "undefined") {
-    sdkWallets.push(new AptosConnectWallet({ network: dappConfig?.network, dappId: dappConfig?.aptosConnectDappId }));
+    sdkWallets.push(
+      new AptosConnectWallet({
+        network: dappConfig?.network,
+        dappId: dappConfig?.aptosConnectDappId,
+      })
+    );
   }
 
   // Push production wallet if env is production, otherwise use dev wallet
@@ -19,6 +25,19 @@ export function getSDKWallets(dappConfig?: DappConfig) {
     sdkWallets.push(new TWallet() as any);
   } else {
     sdkWallets.push(new DevTWallet() as any);
+  }
+
+  if (
+    dappConfig?.network &&
+    [Network.MAINNET, Network.TESTNET].includes(dappConfig.network)
+  ) {
+    sdkWallets.push(
+      new MizuWallet({
+        network: dappConfig.network as any,
+        manifestURL: dappConfig.mizuwallet.manifestURL,
+        appId: dappConfig.mizuwallet.appId,
+      }) as any
+    );
   }
 
   // Add new SDK wallet plugins (ones that should be installed as packages) here:
