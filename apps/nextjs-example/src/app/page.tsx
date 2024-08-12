5715a948ef4cf903b81c0e4fdbd5b2d701e85ad7
bryan-ahnlab <bryan@ahnlab.io>
2024-08-12 14:14:04 +0900

    chore: Remove commented code for registering browser extension wallet plugin

--- apps/nextjs-example/src/app/page.tsx
+++ apps/nextjs-example/src/app/page.tsx
@@ -41,11 +41,11 @@ import { registerWallet } from "@aptos-labs/wallet-standard";
 // Example of how to register a browser extension wallet plugin.
 // Browser extension wallets should call registerWallet once on page load.
 // When you click "Connect Wallet", you should see "Example Wallet"
-/* (function () {
+(function () {
   if (typeof window === "undefined") return;
   const myWallet = new MyWallet();
   registerWallet(myWallet);
-})(); */
+})();
 
 export default function Home() {
   const { account, connected, network, wallet, changeNetwork } = useWallet();
