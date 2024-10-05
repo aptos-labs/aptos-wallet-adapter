a2bdd56801b10ca9f059ed3cda6276a2f273eb71
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2024-10-05 16:54:25 -0700

    [nextjs-example-dapp] Telegram compatibility

--- apps/nextjs-example/src/app/page.tsx
+++ apps/nextjs-example/src/app/page.tsx
@@ -31,6 +31,7 @@ import {
   isAptosNetwork,
   useWallet,
 } from "@aptos-labs/wallet-adapter-react";
+import { init as initTelegram } from "@telegram-apps/sdk";
 import { AlertCircle } from "lucide-react";
 import Image from "next/image";
 
@@ -40,12 +41,17 @@ import { registerWallet } from "@aptos-labs/wallet-standard";
 
 // Example of how to register a browser extension wallet plugin.
 // Browser extension wallets should call registerWallet once on page load.
-// When you click "Connect Wallet", you should see "Example Wallet" 
+// When you click "Connect Wallet", you should see "Example Wallet"
 (function () {
   if (typeof window === "undefined") return;
   const myWallet = new MyWallet();
   registerWallet(myWallet);
 })();
+
+const isTelegramMiniApp = typeof window !== 'undefined' && (window as any).TelegramWebviewProxy !== undefined;
+if (isTelegramMiniApp) {
+  initTelegram();
+}
 
 export default function Home() {
   const { account, connected, network, wallet, changeNetwork } = useWallet();
