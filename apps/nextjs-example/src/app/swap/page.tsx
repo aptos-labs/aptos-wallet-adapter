6478c8a566c81253c87b90781f344c1a3c9aec1f
Blake Zimmerman <blake.zimmerman@icloud.com>
2025-02-05 12:26:42 -0500

    Fix CSS

--- apps/nextjs-example/src/app/swap/page.tsx
+++ apps/nextjs-example/src/app/swap/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { MultiChain } from "@aptos-labs/wallet-adapter-swap";
+import "@aptos-labs/wallet-adapter-swap/dist/index.css";
 
 export default function Swap() {
   return <MultiChain />;
