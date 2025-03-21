5ba8ae3253036f8aa690c77e7959bba5ac8d1779
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2025-03-20 18:00:00 -0700

    [derived-wallet-solana] Improvements and modularization (#511)

---
+++ .changeset/large-plums-jog.md
@@ -0,0 +1,5 @@
+---
+"@aptos-labs/derived-wallet-solana": patch
+---
+
+Extracted `signAptosTransactionWithSolana` and `signAptosStructuredMessageWithSolana`
--- packages/derived-wallet-solana/src/createSiwsEnvelopeForStructuredMessage.ts
+++ packages/derived-wallet-solana/src/createSiwsEnvelopeForStructuredMessage.ts
@@ -1,21 +1,20 @@
 import { aptosChainIdToNetwork, encodeStructuredMessage, StructuredMessage } from '@aptos-labs/derived-wallet-base';
-import {
-  AccountAddressInput,
-  AnyRawTransaction,
-  generateSigningMessageForTransaction,
-  hashValues,
-  Hex,
-} from '@aptos-labs/ts-sdk';
+import { hashValues, Hex } from '@aptos-labs/ts-sdk';
 import { SolanaSignInInputWithRequiredFields } from '@solana/wallet-standard-util';
 import { PublicKey as SolanaPublicKey } from '@solana/web3.js';
 
-export interface CreateSiwsMessageFromAptosStructuredMessageInput {
+export interface CreateSiwsEnvelopeForAptosStructuredMessageInput {
   solanaPublicKey: SolanaPublicKey;
   structuredMessage: StructuredMessage;
 }
 
-export function createSiwsInputFromAptosStructuredMessage(
-  input: CreateSiwsMessageFromAptosStructuredMessageInput,
+/**
+ * Create a SIWS envelope for an Aptos structured message.
+ * A signature on the Solana blockchain by `solanaPublicKey` will be
+ * considered as valid signature on the Aptos blockchain for the provided message.
+ */
+export function createSiwsEnvelopeForAptosStructuredMessage(
+  input: CreateSiwsEnvelopeForAptosStructuredMessageInput,
 ): SolanaSignInInputWithRequiredFields {
   const { solanaPublicKey, structuredMessage } = input;
   const {
@@ -46,45 +45,6 @@ export function createSiwsInputFromAptosStructuredMessage(
   const messageHash = hashValues([encodedMessage]);
   // TODO: consider using b58 or b64 instead
   const requestId = Hex.fromHexInput(messageHash).toStringWithoutPrefix();
-
-  return {
-    address: solanaPublicKey.toString(),
-    domain: window.location.host,
-    uri: window.location.origin,
-    nonce,
-    requestId,
-    statement,
-    version: '1',
-  };
-}
-
-export interface CreateSiwsMessageFromAptosTransaction {
-  solanaPublicKey: SolanaPublicKey;
-  aptosAddress: AccountAddressInput;
-  rawTransaction: AnyRawTransaction;
-}
-
-export function createSiwsInputFromAptosTransaction(
-  input: CreateSiwsMessageFromAptosTransaction,
-): SolanaSignInInputWithRequiredFields {
-  const { solanaPublicKey, aptosAddress, rawTransaction } = input;
-  const signingMessage = generateSigningMessageForTransaction(rawTransaction);
-  const messageHash = hashValues([signingMessage]);
-  const messageHashHex = Hex.fromHexInput(messageHash).toStringWithoutPrefix();
-
-  // TODO: consider using b58 or b64 instead
-  const requestId = messageHashHex;
-  const nonce = messageHashHex;
-
-  const chainId = rawTransaction.rawTransaction.chain_id.chainId;
-
-  // Attempt to convert chainId into a human-readable identifier
-  const networkId = aptosChainIdToNetwork(chainId) ?? `chainId: ${chainId}`;
-
-  const onAptosNetwork = ` on Aptos (${networkId})`;
-  const asAccount = ` with account ${aptosAddress.toString()}`;
-  // TODO: define a good way to display the transaction
-  const statement = `Sign the following transaction${onAptosNetwork}${asAccount}: TODO`;
 
   return {
     address: solanaPublicKey.toString(),
