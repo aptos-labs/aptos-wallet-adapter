231eb7595d9f44fbc1d1f07613b0f22f48d2da8c
maayan <maayansavir@gmail.com>
2025-03-27 14:13:59 -0400

    Updates

--- nextjs-cross-chain-example/_next/static/chunks/pages/_app-48bdf47a2e30c15b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{68362:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(30714)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(68362),_(98371)}),_N_E=n.O()}]);
