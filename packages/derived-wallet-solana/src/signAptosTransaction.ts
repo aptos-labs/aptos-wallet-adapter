27daa6dfbd00457dac41fc904e25a89e1b76b997
maayan <maayansavir@gmail.com>
2025-04-04 19:15:28 -0400

    modify signature sent to auth function

--- packages/derived-wallet-solana/src/signAptosTransaction.ts
+++ packages/derived-wallet-solana/src/signAptosTransaction.ts
@@ -6,11 +6,18 @@ import {
   Ed25519Signature,
   generateSigningMessageForTransaction,
   hashValues,
+  Serializer,
 } from '@aptos-labs/ts-sdk';
 import { StandardWalletAdapter as SolanaWalletAdapter } from "@solana/wallet-standard-wallet-adapter-base";
 import { createSiwsEnvelopeForAptosTransaction } from './createSiwsEnvelope';
 import { wrapSolanaUserResponse } from './shared';
 
+/**
+ * A first byte of the signature that indicates the "message type", this is defined in the
+ * authentication function on chain, and lets us identify the type of the message and to make
+ * changes in the future if needed.
+ */
+export const SIGNATURE_TYPE = 0;
 export interface SignAptosTransactionWithSolanaInput {
   solanaWallet: SolanaWalletAdapter;
   authenticationFunction: string;
@@ -44,17 +51,25 @@ export async function signAptosTransactionWithSolana(input: SignAptosTransaction
     if (output.signatureType && output.signatureType !== 'ed25519') {
       throw new Error('Unsupported signature type');
     }
+
     // The wallet might change some of the fields in the SIWS input, so we
     // might need to include the finalized input in the signature.
     // For now, we can assume the input is unchanged.
     const signature = new Ed25519Signature(output.signature);
 
+    // Serialize the signature with the signature type as the first byte.
+    const serializer = new Serializer();
+    serializer.serializeU8(SIGNATURE_TYPE);
+    serializer.serializeBytes(signature.toUint8Array());
+    const abstractSignature = serializer.toUint8Array();
+
+    // Serialize the abstract public key.
     const abstractPublicKey = new DerivableAbstractPublicKey(solanaPublicKey.toBase58(), domain);
 
     return new AccountAuthenticatorAbstraction(
       authenticationFunction,
       signingMessageDigest,
-      signature.toUint8Array(),
+      abstractSignature,
       abstractPublicKey.bcsToBytes()
     );
   });
