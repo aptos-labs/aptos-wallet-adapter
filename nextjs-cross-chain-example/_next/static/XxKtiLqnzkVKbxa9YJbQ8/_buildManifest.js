65a53d02a9a6907f95863ee3928a8cad7e45dd56
GhostWalker562 <43276017+GhostWalker562@users.noreply.github.com>
2025-07-08 11:31:59 -0700

    Updates

--- nextjs-cross-chain-example/_next/static/XxKtiLqnzkVKbxa9YJbQ8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-c9a74f763fee2d7f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
