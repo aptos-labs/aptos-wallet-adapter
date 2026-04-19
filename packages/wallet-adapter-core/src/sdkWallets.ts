664d1a33757abcc4356f87eb8ed4ed6d87b86340
ARYASH GUPTA <aryashgupta281@gmail.com>
2026-04-19 15:17:50 +0530

    fix: apply chain ID deduplication in @aptos-connect plugin instead of monkey-patching

--- packages/wallet-adapter-core/src/sdkWallets.ts
+++ packages/wallet-adapter-core/src/sdkWallets.ts
@@ -3,32 +3,7 @@ import {
   AptosConnectGenericWallet,
   AptosConnectGoogleWallet,
 } from "@aptos-connect/wallet-adapter-plugin";
-import { Aptos } from "@aptos-labs/ts-sdk";
 import type { AdapterWallet, DappConfig } from "./WalletCore";
-
-function withDedupedChainIdPrefetch<T>(callback: () => T): T {
-  const originalGetChainId = Aptos.prototype.getChainId;
-  let sharedChainIdPromise: ReturnType<typeof originalGetChainId> | undefined;
-
-  // Aptos Connect eagerly prefetches the chain ID in each wallet constructor.
-  // Reuse the first request during SDK wallet creation so we only hit the node once.
-  Aptos.prototype.getChainId = function getDedupedChainId(this: Aptos) {
-    if (!sharedChainIdPromise) {
-      sharedChainIdPromise = originalGetChainId.call(this).catch((error) => {
-        sharedChainIdPromise = undefined;
-        throw error;
-      });
-    }
-
-    return sharedChainIdPromise;
-  };
-
-  try {
-    return callback();
-  } finally {
-    Aptos.prototype.getChainId = originalGetChainId;
-  }
-}
 
 export function getSDKWallets(dappConfig?: DappConfig) {
   const sdkWallets: AdapterWallet[] = [];
@@ -42,11 +17,9 @@ export function getSDKWallets(dappConfig?: DappConfig) {
     };
 
     sdkWallets.push(
-      ...withDedupedChainIdPrefetch(() => [
-        new AptosConnectGoogleWallet(aptosConnectConfig),
-        new AptosConnectAppleWallet(aptosConnectConfig),
-        new AptosConnectGenericWallet(aptosConnectConfig),
-      ]),
+      new AptosConnectGoogleWallet(aptosConnectConfig),
+      new AptosConnectAppleWallet(aptosConnectConfig),
+      new AptosConnectGenericWallet(aptosConnectConfig),
     );
   }
 
